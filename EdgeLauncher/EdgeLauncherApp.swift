98e2b76080570ae1819c1c11c8f34acb361cf70f
박종영 <parkjy@cosmax.com>
2026-05-15 01:34:36 +0900

    Initial Commit

---
+++ EdgeLauncher/EdgeLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeLauncherApp.swift
+//  EdgeLauncher
+//
+//  Created by jongyoung park on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct EdgeLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
