c44a2d535bb1d19a180da1f98cc2f5ecf0494a4b
박종영 <parkjy@cosmax.com>
2026-05-15 02:02:10 +0900

    feat: add YouTubeMusicWebView wrapper

---
+++ EdgeLauncher/Modules/YouTubeMusic/YouTubeMusicWebView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+import WebKit
+
+struct YouTubeMusicWebView: NSViewRepresentable {
+    let url: URL
+
+    func makeNSView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        config.websiteDataStore = .default()
+        config.mediaTypesRequiringUserActionForPlayback = []
+        config.allowsAirPlayForMediaPlayback = true
+
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.allowsBackForwardNavigationGestures = true
+        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15"
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        if nsView.url != url {
+            nsView.load(URLRequest(url: url))
+        }
+    }
+}
